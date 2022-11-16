ce021d96bb857b5e83de66f84decd501b0d87f5c
Johnston Yang <jya121@sfu.ca>
2022-11-15 21:06:46 -0800

    adding supporting code for testbench

--- Simulation/TBDecoder.vhd
+++ Simulation/TBDecoder.vhd
@@ -12,7 +12,7 @@ end decoder_tb;
 
 architecture sim of decoder_tb is
 
-    -- TODO: Create the Decoder text file for test cases
+    -- TODO: Next steps, wait until allout is finished and compare the values
 	constant TestVectorFile : string := "DecoderUnit00.txt";
 	constant ClockPeriod : time := 2 ns;
 	constant ResetPeriod : time := 5 ns;
@@ -31,6 +31,17 @@ architecture sim of decoder_tb is
     signal rs2 : std_logic_vector(4 downto 0);
     signal rd : std_logic_vector(4 downto 0);
 
+    signal allout : std_logic_vector(31 downto 0);
+
+    -- testbench signals
+    signal tb_opcode : std_logic_vector(6 downto 0); 
+    signal tb_funct7 : std_logic_vector(6 downto 0);
+    signal tb_funct3 : std_logic_vector(2 downto 0);
+    signal tb_rs1 : std_logic_vector(4 downto 0);
+    signal tb_rs2 : std_logic_vector(4 downto 0);
+    signal tb_rd : std_logic_vector(4 downto 0);
+
+
     Signal MeasurementIndex : Integer := 0;
 	File   VectorFile : text; 
 
@@ -40,6 +51,8 @@ begin
 	Resetn <= '1', '0' after ResetPeriod;
 	Sstable <= opcode'stable(PostStimTime);
 	Squiet <= opcode'quiet(PostStimTime);
+
+    allout <= opcode & funct7 & funct3 & rs1 & rs2 & rd;
 
     DUT : entity work.decoder(rtl) generic map (N => N)
     port map (
@@ -73,7 +86,40 @@ begin
         report "Using TestVectors from file " & TestVectorFile;
 
         while not endfile( VectorFile ) loop
+            -- set values to 'X'
+            MeasurementIndex <= MeasurementIndex + 1;
+            instruction <= (others => 'X');
+            opcode <= (others => 'X');
+            funct7 <= (others => 'X');
+            funct3 <= (others => 'X');
+            rs1 <= (others => 'X');
+            rs2 <= (others => 'X');
+            rd <= (others => 'X');
 
+            ResultV := 'X';
+            PropTimeDelay := 0 ns;
+            wait for PreStimTime;
+            -- setup input values
+            StartTime := NOW;
+            ResultV := '1';
+            readline(VectorFile, LineBuffer);
+            hread(LineBuffer, instVar);
+            hread(LineBuffer, sevenVar);
+            hread(LineBuffer, rs2Var);
+            hread(LineBuffer, rs1Var);
+            hread(LineBuffer, threeVar);
+            hread(LineBuffer, rdVar);
+            hread(LineBuffer, opVar);
+
+            instruction <= instVar;
+            tb_opcode <= opVar;
+            tb_funct7 <= sevenVar;
+            tb_funct3 <= threeVar;
+            tb_rs1 <= rs1Var;
+            tb_rs2 <= rs2Var;
+            tb_rd <= rdVar;
+
+            wait until 
         end loop;
     end process;
 
